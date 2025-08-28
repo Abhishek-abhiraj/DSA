bccf685f78555cbffa495fdcaf52556bda5e14eb
Abhishek Abhiraj <abhishekabhirajemail@gmail.com>
2025-08-28 20:05:26 +0530

    conditional statement

---
+++ conditional-statement/lowercase_and_uppercase.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  char ch;
+  cout<<"enter the character ";
+  cin>>ch;
+  if(ch>='a' && ch<='z'){
+    cout<<ch<<"is a lowercase Alphabet";
+  }
+  else{
+    cout<<ch<<"is a uppercase Alphabet";
+  }
+  return 0;
+}
